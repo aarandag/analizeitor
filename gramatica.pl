0e4b98a1fbf2aeb4be9cd4ae4d9c45540f958bb0
catpilo <catpilo@users.noreply.github.com>
2017-04-18 18:50:08 +0200

    No se va a la puta

--- gramatica.pl
+++ gramatica.pl
@@ -1,5 +1,5 @@
 %% Reglas gramaticales
-programa(X):-tokenizer(X,B),phrase(oracion,B).
+programa(X):-downcase_atom(X, D), tokenizer(D,B),phrase(oracion,B).
 oracion --> s_nominal(_, _), s_verbal.
 
 s_nominal(Genero, Numero) --> determinante(Genero, Numero), s_nominal2(Genero, Numero).
@@ -8,7 +8,7 @@ s_nominal(Genero, Numero) --> s_nominal2(Genero, Numero).
 s_nominal2(G, N) --> sustantivo(G, N).
 
 s_nominal2(G, N) --> adjetivo(G, N), s_nominal(G, N).
-%% s_nominal2(G, N) --> s_nominal(G, N), adjetivo(G, N). @@@La pila se va a la puta :=(
+s_nominal2(G, N) --> sustantivo(G, N), adjetivo(G, N).% @@@La pila :=(
 
 s_verbal --> verbo.
 s_verbal --> verbo, s_nominal(_, _).
